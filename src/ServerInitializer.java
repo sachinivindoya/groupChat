10b94938cb573e2ecd39a051b69396f13c67bc28
sachinivinodya <vinodyasachinivinodya@gmail.com>
2023-05-23 02:58:26 +0530

    implement_Server

---
+++ src/ServerInitializer.java
@@ -0,0 +1,15 @@
+package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
